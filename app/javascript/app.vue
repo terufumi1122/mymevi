a3712575728b5896ac177da4120c41abfe81a6f8
terufumi1122 <terufumi1122@gmail.com>
2019-12-23 17:20:53 +0900

    first commit

---
+++ app/javascript/app.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app id="app">
+    <Header/>
+    <h1>This is app.vue</h1>
+    <p>{{ message }}</p>
+    <Top/>
+  </v-app>
+</template>
+
+<script>
+import Top from "./components/Top";
+import Header from "./components/Header";
+
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!"
+    }
+  },
+  components: {
+    Header,
+    Top,
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
---
+++ app/javascript/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header id="header">
+    <v-app-bar>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>This is Header.vue</v-toolbar-title>
+    </v-app-bar>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+  }
+</script>
+
+<style>
+</style>
