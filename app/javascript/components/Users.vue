2cc04ac666c4bb1d285ffaa5abf35e80d3352f5c
terufumi1122 <terufumi1122@gmail.com>
2019-12-25 13:18:56 +0900

    add User.vue

---
+++ app/javascript/components/Users.vue
@@ -0,0 +1,39 @@
+<template>
+  <table>
+    <tbody>
+      <tr>
+        <th>ID</th>
+        <th>name</th>
+        <th>email</th>
+        <th>age</th>
+        <th>genter</th>
+      </tr>
+      <tr v-for="user in users" :key="user.id">
+        <td>{{ user.id }}</td>
+        <td>{{ user.name }}</td>
+        <td>{{ user.email }}</td>
+        <td>{{ user.age }}</td>
+        <td>{{ user.gender }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: "Users",
+  data() {
+    return {
+      users: []
+    }
+  },
+  mounted() {
+    axios
+      .get('/api/v1/users.json')
+      .then(response => (this.users = response.data))
+  }
+}
+</script>
+
