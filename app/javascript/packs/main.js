66f2a479e1634c3231553f6b96fb263a3204e7df
terufumi1122 <terufumi1122@gmail.com>
2020-01-05 15:34:16 +0900

    delete unused console.log

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -18,8 +18,4 @@ document.addEventListener('DOMContentLoaded', () => {
     render: h => h(App)
   }).$mount()
   document.body.appendChild(app.$el)
-
-  console.log(app)
-  console.log(store)
-  console.log(store.state.count)
 })
