1aa889971b554a6aad4fab5d2c80923f59950865
terufumi1122 <terufumi1122@gmail.com>
2020-01-09 16:19:31 +0900

    remove likes_column from habits 2

--- app/controllers/api/v1/habits_controller.rb
+++ app/controllers/api/v1/habits_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::HabitsController < ApiController
   before_action :set_current_user_habits, only: [:users_habits_show]
-  before_action :set_habit, only: [:like, :unlike]
 
   def all_habits_show
     all_habits = Habit.joins(:user)
@@ -9,7 +8,6 @@ class Api::V1::HabitsController < ApiController
       habits.name,
       description,
       best,
-      likes,
       user_id,
       users.name AS user_name,
       users.birthday AS user_birthday,
@@ -31,31 +29,13 @@ class Api::V1::HabitsController < ApiController
     end
   end
 
-  def like
-    @current_habit.likes ++
-    unless @current_habit.save
-      render json: { errors: @current_habit.errors.full_messages }
-    end
-  end
-  
-  def unlike
-    @current_habit.likes --
-    unless @current_habit.save
-      render json: { errors: @current_habit.errors.full_messages }
-    end
-  end
-
   private
-
-  def set_habit
-    @current_habit = Habit.find(params[:id])
-  end
 
   def set_current_user_habits
     @current_user_habits = Habit.where(user_id: params[:user_id])
   end
 
   def habit_params
-    params.permit(:name, :description, :best, :user_id, :likes)
+    params.permit(:name, :description, :best, :user_id)
   end
 end
