a23139d646d0d73121c4d31eb49297acd4fe89ef
terufumi1122 <terufumi1122@gmail.com>
2020-01-10 12:58:26 +0900

    create favorite API

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,4 +1,27 @@
-class Api::V1::FavoritesController < ApplicationController
+class Api::V1::FavoritesController < ApiController
+
+  before_action :set_favorites, only: [:index]
+
   def index
+    render json: @favorites
+  end
+
+  def create
+    favorite = Favorite.new(favorite_params)
+    if favorite.save
+      render json: favorite, status: :created
+    else
+      render json: { errors: favorite.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_favorites
+    @favorites = Favorite.all
+  end
+
+  def favorite_params
+    params.permit(:user_id, :habit_id)
   end
 end
