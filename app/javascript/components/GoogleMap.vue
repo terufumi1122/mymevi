d71911b5d8803b9be2c3fd97aab8bd26bf0c19a5
terufumi1122 <terufumi1122@gmail.com>
2020-01-31 12:36:44 +0900

    fix bug in GoogleMap.vue

--- app/javascript/components/GoogleMap.vue
+++ app/javascript/components/GoogleMap.vue
@@ -7,10 +7,10 @@
     </v-row>
     <SpeedDial
       mainIcon="mdi-google-maps"
-      icon1="mdi-map-marker-plus"
+      icon1="mdi-map-marker-radius"
       color1="blue"
       :click1="setCurrentLocation"
-      icon2="mdi-map-marker"
+      icon2="mdi-map-marker-multiple"
       color2="teal"
       :click2="setMarker"
       icon3="mdi-delete"
@@ -110,66 +110,19 @@ export default {
         disableDefaultUI: true,
         zoomControl: true
       };
+
+      //描画するためのmapを新規作成
       this.map = new google.maps.Map(target, mapOptions);
-      let geocoder = new google.maps.Geocoder();
 
       // クリックしたらマーカーを配置し、位置情報をDB記録
+
+      //mapをクリックしたときの動作を定義
       this.map.addListener("click", e => {
-        //Geocoderで緯度経度を住所に変換する
-        geocoder.geocode(
-          {
-            location: e.latLng
-          },
-          (results, status) => {
-            if (status !== "OK") {
-              alert("Failed: " + status);
-              return;
-            }
-            // 該当位置の住所：results[0].formatted_address
-            if (results[0]) {
-              this.address = results[0].formatted_address;
-              //検証用
-              console.log(this.address);
-            } else {
-              alert("No results!");
-              return;
-            }
-          }
-        );
-        //マーカーを定義する
-        let marker = new google.maps.Marker({
-          position: e.latLng,
-          map: this.map,
-          title: this.address,
-          animation: google.maps.Animation.DROP
-        });
-        //マーカーをクリックしたときのウインドウに表示する内容を定義する
-        let infoWindow = new google.maps.InfoWindow({
-          content: e.latLng.toString()
-        });
-        //マーカーをクリックしたらウインドウが開くようにする
-        marker.addListener("click", () => {
-          infoWindow.open(this.map, marker);
-        });
-
-        //動作確認用（最終的には消す）
-        console.log(e.latLng);
-        console.log(e.latLng.lat());
-        console.log(e.latLng.lat().toString());
-
-        if (this.currentUser) {
-          //位置情報を記録するために変数を宣言
-          let locationParams = {
-            // name: e.latLng.toString(),
-            name: this.address,
-            lat: e.latLng.lat(),
-            lng: e.latLng.lng(),
-            user_id: this.currentUser.id,
-            habit_id: 0
-          };
-          //location.jsで定義したactionsを呼び出す。
-          this.addLocation(locationParams);
-        }
+          this.getAddress(e) //緯度経度から文字列の住所に変換して情報を取得する
+          setTimeout(() => {
+            this.addMarker(e) //マーカーを追加する
+            this.setLocationParams(e) //DBに位置情報を保存する
+          }, 500) //500msで動作支障なし ※要変更
       });
     },
 
@@ -179,7 +132,7 @@ export default {
         alert("Geolocation not supported!");
         return;
       }
-
+      console.log('setCurrentLocation()を開始します。') //後で消す
       //現在位置の取得を行う
       navigator.geolocation.getCurrentPosition(
         position => {
@@ -188,16 +141,12 @@ export default {
             lat: position.coords.latitude,
             lng: position.coords.longitude
           };
-          console.log(this.center);
-          // 新しいMapを描画する
-          // 今のMapに描画すべきなので変更する。
-          let currentMap = new google.maps.Map(target, {
-            center: this.center,
-            zoom: 15
-          });
+          console.log(this.center); //後で消す
+
+          this.map.panTo(this.center) //現在位置にmapの表示位置を移動させる
           new google.maps.Marker({
             position: this.center,
-            map: currentMap,
+            map: this.map,
             title: "現在位置",
             animation: google.maps.Animation.DROP
           });
@@ -221,6 +170,71 @@ export default {
           return;
         }
       );
+    },
+
+    //以下、長くなるので切り出した関数
+
+    getAddress(e) {
+        new google.maps.Geocoder().geocode({ location: e.latLng },
+        (results, status) => {
+          const regExp = /〒\d{3}-\d{4} (.+)/
+        const address = (results[0].formatted_address).match(regExp)
+        console.log(status)
+        if (status !== "OK") {
+          alert("Failed: " + status);
+          return;
+        }
+        // 該当位置の住所：results[0].formatted_address
+        if (results[0]) {
+          // this.address = results[0].formatted_address;
+          this.address = address[1];
+          console.log(`this.addressは`)
+          //検証用
+          console.log(this.address);
+        } else {
+          alert("No results!");
+          return;
+        }
+      })
+    },
+    addMarker(e) {
+        //マーカーを定義する
+        let marker = new google.maps.Marker({
+          position: e.latLng,
+          map: this.map,
+          title: this.address,
+          animation: google.maps.Animation.DROP
+        });
+        //マーカーをクリックしたときのウインドウに表示する内容を定義する
+        let infoWindow = new google.maps.InfoWindow({
+          content: e.latLng.toString()
+        });
+        //マーカーをクリックしたらウインドウが開くようにする
+        marker.addListener("click", () => {
+          infoWindow.open(this.map, marker);
+        });
+    },
+    setLocationParams(e) {
+        if (this.currentUser) {
+          //位置情報を記録するために変数を宣言
+        console.log(`currentUserは${this.currentUser}です`)
+        let locationParams = {
+          name: this.address,
+          lat: e.latLng.lat(),
+          lng: e.latLng.lng(),
+          user_id: this.currentUser.id,
+          habit_id: 0
+        };
+        console.log(`locationParamsは${locationParams}です`)
+        console.log(locationParams)
+
+        if (locationParams.name === null) {
+          alert('住所情報の取得に失敗しました。処理を中断します。')
+          return
+        }
+        //location.jsで定義したactionsを呼び出す。
+        this.addLocation(locationParams);
+      }
     }
   }
 };
