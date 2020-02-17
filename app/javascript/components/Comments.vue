fb8d4b72b3c5b68f2a1bb9fbf45ff2131dcb2ca2
terufumi1122 <terufumi1122@gmail.com>
2020-02-17 10:34:56 +0900

    restrict CommentEdit

--- app/javascript/components/Comments.vue
+++ app/javascript/components/Comments.vue
@@ -1,49 +1,49 @@
 <template>
   <div>
-    <div v-for="comment in comments" :key="comment.id">
-      <v-card
-        class="mx-auto my-3"
-        max-width="374"
-      >
-        <div class="balloon">
-          <v-card-text
-            class="text--black font-weight-bold text-center"
-          >
-            {{ comment.content }}
-          </v-card-text>
-        </div>
-
-        <v-card-actions>
-          <v-list-item class="grow">
-
-            <v-list-item-avatar color="grey darken-3">
-              <img
-                class="elevation-6"
-                :src="avatar(comment.user_id)"
-              />
-            </v-list-item-avatar>
-
-            <v-list-item-content>
-              <v-list-item-subtitle>
-                {{ comment.user_name }} さん
-              </v-list-item-subtitle>
-            </v-list-item-content>
-
-            <div
-              @click="setComment(comment)"
+    <v-lazy
+      :options="{ threshold: .5 }"
+      transition="fade-transition"
+    >
+      <div v-for="comment in comments" :key="comment.id">
+        <v-card
+          class="mx-auto my-3"
+          max-width="374"
+        >
+          <div class="balloon">
+            <v-card-text
+              class="text--black font-weight-bold text-center"
             >
-              <v-btn
-                text
-                color="deep-purple accent-4"
-                :to="{ name: 'CommentEdit'}"
-              >編集</v-btn>
-            </div>
-
-          </v-list-item>
-        </v-card-actions>
-      </v-card>
-    </div>
-
+              {{ comment.content }}
+            </v-card-text>
+          </div>
+          <v-card-actions>
+            <v-list-item class="grow">
+              <v-list-item-avatar color="grey darken-3">
+                <img
+                  class="elevation-6"
+                  :src="avatar(comment.user_id)"
+                />
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-subtitle>
+                  {{ comment.user_name }} さん
+                </v-list-item-subtitle>
+              </v-list-item-content>
+              <div
+                v-if="comment.user_id === currentUser.id"
+                @click="setComment(comment)"
+              >
+                <v-btn
+                  text
+                  color="deep-purple accent-4"
+                  :to="{ name: 'CommentEdit'}"
+                >編集</v-btn>
+              </div>
+            </v-list-item>
+          </v-card-actions>
+        </v-card>
+      </div>
+    </v-lazy>
   </div>
 </template>
 
@@ -57,6 +57,7 @@
         'comments',
         'avatars',
         'avatar',
+        'currentUser'
       ]),
     },
     methods: {
