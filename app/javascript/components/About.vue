b61fc412bd25bc1ee42f700e52d935c349515f44
terufumi1122 <terufumi1122@gmail.com>
2020-02-19 08:49:54 +0900

    remove <v-content>

--- app/javascript/components/About.vue
+++ app/javascript/components/About.vue
@@ -1,95 +1,93 @@
 <template>
-  <v-content>
-    <v-container class="mx-auto text-center">
+  <v-container class="mx-auto text-center">
+    <v-row>
+      <v-spacer></v-spacer>
+      <img
+        v-if="currentUser === null"
+        src="../../assets/images/002-infinity.svg"
+        alt="メインロゴ"
+        class="main__logo"
+      >
+      <img
+        v-else
+        src="../../assets/images/002-infinity.svg"
+        alt="メインロゴ"
+        class="main__logo main__logo--after"
+      >
+      <v-spacer></v-spacer>
+    </v-row>
+    <v-row>
+      <v-spacer></v-spacer>
+      <p><strong>マイメビ</strong>は、自分の<strong>イチオシ習慣</strong>を<strong>匿名</strong>でシェア出来るサービスです。</p>
+      <v-spacer></v-spacer>
+    </v-row>
+    <v-container v-if="currentUser === null">
       <v-row>
         <v-spacer></v-spacer>
-        <img
-          v-if="currentUser === null"
-          src="../../assets/images/002-infinity.svg"
-          alt="メインロゴ"
-          class="main__logo"
-        >
-        <img
-          v-else
-          src="../../assets/images/002-infinity.svg"
-          alt="メインロゴ"
-          class="main__logo main__logo--after"
-        >
+          <v-btn
+            color="#0f4c81"
+            dark
+            :to="{ name: 'LoginForm' }"
+          >
+            ログイン
+          </v-btn>
+        <v-spacer></v-spacer>
+          <v-btn
+            color="#278040"
+            dark
+            @click="guestLogin"
+          >
+            簡単ログイン
+          </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="#990011"
+            dark
+            :to="{ name: 'SignUpForm' }"
+          >
+            新規登録
+          </v-btn>
         <v-spacer></v-spacer>
       </v-row>
-      <v-row>
-        <v-spacer></v-spacer>
-        <p><strong>マイメビ</strong>は、自分の<strong>イチオシ習慣</strong>を<strong>匿名</strong>でシェア出来るサービスです。</p>
-        <v-spacer></v-spacer>
-      </v-row>
-      <v-container v-if="currentUser === null">
-        <v-row>
-          <v-spacer></v-spacer>
-            <v-btn
-              color="#0f4c81"
-              dark
-              :to="{ name: 'LoginForm' }"
-            >
-              ログイン
-            </v-btn>
-          <v-spacer></v-spacer>
-            <v-btn
-              color="#278040"
-              dark
-              @click="guestLogin"
-            >
-              簡単ログイン
-            </v-btn>
-            <v-spacer></v-spacer>
-            <v-btn
-              color="#990011"
-              dark
-              :to="{ name: 'SignUpForm' }"
-            >
-              新規登録
-            </v-btn>
-          <v-spacer></v-spacer>
-        </v-row>
-      </v-container>
-      <v-container v-else>
-        <v-row>
-          <div class="mx-auto">
-            <h2>早速使ってみよう！</h2>
-          </div>
-        </v-row>
-        <v-row>
-          <v-btn
-            class="mx-auto mb-3"
-            color="green"
-            dark
-            :to="{ name: 'UserDetail' }"
-          >
-            <v-icon>mdi-account-circle</v-icon>:自分のプロフィール・習慣
-          </v-btn>
-        </v-row>
-        <v-row>
-          <v-btn
-            class="mx-auto mb-3"
-            color="green darken-2"
-            dark
-            :to="{ name: 'AllBestHabits' }"
-          >
-            <v-icon>mdi-heart</v-icon>:みんなのオススメ習慣
-          </v-btn>
-        </v-row>
-        <v-row>
-          <v-btn
-            class="mx-auto mb-3"
-            color="green darken-4"
-            dark
-            :to="{ name: 'GoogleMap' }"
-          >
-            <v-icon>mdi-map-marker</v-icon>:みんなの定番スポット
-          </v-btn>
-        </v-row>
-      </v-container>
     </v-container>
-  </v-content>
+    <v-container v-else>
+      <v-row>
+        <div class="mx-auto">
+          <h2>早速使ってみよう！</h2>
+        </div>
+      </v-row>
+      <v-row>
+        <v-btn
+          class="mx-auto mb-3"
+          color="green"
+          dark
+          :to="{ name: 'UserDetail' }"
+        >
+          <v-icon>mdi-account-circle</v-icon>:自分のプロフィール・習慣
+        </v-btn>
+      </v-row>
+      <v-row>
+        <v-btn
+          class="mx-auto mb-3"
+          color="green darken-2"
+          dark
+          :to="{ name: 'AllBestHabits' }"
+        >
+          <v-icon>mdi-heart</v-icon>:みんなのオススメ習慣
+        </v-btn>
+      </v-row>
+      <v-row>
+        <v-btn
+          class="mx-auto mb-3"
+          color="green darken-4"
+          dark
+          :to="{ name: 'GoogleMap' }"
+        >
+          <v-icon>mdi-map-marker</v-icon>:みんなの定番スポット
+        </v-btn>
+      </v-row>
+    </v-container>
+  </v-container>
 </template>
 
 <script>
