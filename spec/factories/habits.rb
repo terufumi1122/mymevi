e079968e6da5f198cd657ea2bb01908043af4ff0
terufumi1122 <terufumi1122@gmail.com>
2020-02-19 19:21:34 +0900

    add merit to habits.rb

--- spec/factories/habits.rb
+++ spec/factories/habits.rb
@@ -17,6 +17,7 @@ FactoryBot.define do
   factory :habit do
     name               { Faker::Lorem.sentence }
     description        { Faker::Lorem.paragraph }
+    merit              { Faker::Lorem.sentence }
     best               { rand(1..4) }
     sequence(:user_id) { |n| n }
 
